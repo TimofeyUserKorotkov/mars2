5e197ba134a0be30b26a3a9e6c90cd79c1bc733f
Milanayusupova <adriana.yusupova@yandex.ru>
2024-02-19 19:24:56 +0300

    2

--- main.py
+++ main.py
@@ -4,9 +4,10 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("/training/<title>")
-def index(prof):
-    return render_template("training.html", prof=prof.lower())
+@app.route("/list_prof/<lst>")
+def index(lst):
+    professions = ["инженер", "строитель", "врач", "пожарный"]
+    return render_template("/static/html/list_prof.html", lst=lst, professions=professions)
 
 
 if __name__ == "__main__":
