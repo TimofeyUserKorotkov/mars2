138fd07d80a7171d745d0b4f638fa1e947e0e0ca
Milanayusupova <adriana.yusupova@yandex.ru>
2024-02-22 18:34:13 +0300

    forms-1

--- main.py
+++ main.py
@@ -1,13 +1,35 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, redirect
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
-@app.route("/list_prof/<lst>")
-def index(lst):
-    professions = ["инженер", "строитель", "врач", "пожарный"]
-    return render_template("/static/html/list_prof.html", lst=lst, professions=professions)
+class LoginForm(FlaskForm):
+    user_id = StringField('Id астронавта', validators=[DataRequired()])
+    password_1 = PasswordField('Пароль', validators=[DataRequired()])
+    cap_id = StringField('Id капитана', validators=[DataRequired()])
+    password_2 = PasswordField('Пароль', validators=[DataRequired()])
+    submit = SubmitField('Доступ')
+
+
+@app.route("/login", method=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect("/succes")
+
+
+@app.route("/")
+def index():
+    return redirect("/login")
+
+
+@app.route("/success")
+def success():
+    return render_template("success.html")
 
 
 if __name__ == "__main__":
